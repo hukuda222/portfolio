bf61688d1d33954fe3375c989bad41459c3961fa
ShogoFujita <fujita@ShogoFujitanoMacBook-Pro.local>
2019-07-29 10:28:58 +0900

    first

---
+++ hello.md
@@ -0,0 +1,3 @@
+# hello
+## poyo
+- unko
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        hukuda222
+      </h1>
+      <h2 class="subtitle">
+        My good Nuxt.js project
+      </h2>
+    </div>
+  <div v-html="hello"></div>
+  <ul>
+    <li v-for="data in datas" :key=data.path >{{ data.path }}</li>
+  </ul>
+
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import hello from '../hello.md'
+import article_data from '../article/data.json'
+
+
+export default {
+  computed: {
+      hello() {
+        return hello
+      }
+  },
+  data:()=>{
+    return {
+      datas:article_data
+    }
+  }
+}
+</script>
+
+
