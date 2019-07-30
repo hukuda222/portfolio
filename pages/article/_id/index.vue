e6d1e13615a5479c975f8f48cce09bf130f12c0c
ShogoFujita <fujita@ShogoFujitanoMacBook-Pro.local>
2019-07-30 13:50:01 +0900

    記事とかが読めるようになったはず

--- pages/article/_id/index.vue
+++ pages/article/_id/index.vue
@@ -8,15 +8,29 @@
         My good Nuxt.js project
       </h2>
     </div>
-    <div>{{ $route.params.id }}</div>
+    <div v-html="$md.render(article_data)"></div>
   </div>
 </template>
 
 <script>
-
+import axios from 'axios';
 
 export default {
+  validate({params}) {
+    return /^[0-9a-z]+$/.test(params.id)
+  },
+  async asyncData({ params }) {    
+    
+    console.log("https://raw.githubusercontent.com/hukuda222/portfolio/master/article/${params.id}.md");
+    const { data: res } = await axios.get(`https://raw.githubusercontent.com/hukuda222/portfolio/master/article/${params.id}.md`);
+    self.article_data=res;
 
+  },
+  data:()=>{
+    return {
+      article_data:""
+    }
+  }
 }
 </script>
 
--- pages/article_list/index.vue
+++ pages/article_list/index.vue
@@ -1,45 +1,33 @@
 <template>
   <div class="container">
-    <nuxt-link to="/"></nuxt-link>
     <div>
       <h1 class="title">
         hukuda222
       </h1>
-      <h2 class="subtitle">
-        My good Nuxt.js project
-      </h2>
     </div>
-  <div v-html="top"></div>
-  <!--div v-html="$md.render(model)"></div-->
   <ul>
-    <li v-for="data in datas" :key=data.path >{{ data.path }}</li>
+    <li v-for="article in article_list" :key=article.path >
+      <a v-bind:href="'/article/'+article.path">
+            {{ article.title }}
+      </a>
+    </li>
   </ul>
 
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import article_data from '../article/data.json'
 import axios from 'axios';
 
 
 export default {
-  computed: {
-      top() {
-        return top 
-      }
-  }/*,
   async mounted() {
-    const { data: model } = await axios.get("https://raw.githubusercontent.com/hukuda222/ex3-emulator/master/README.md",{transformResponse: d => d})
-    this.model = model
-  },
-  data:()=>{
-    return {
-      datas:article_data,
-      model:""
-    }
-  }*/
+     const { data: res } = await axios.get("https://raw.githubusercontent.com/hukuda222/portfolio/master/article/data.json");
+     this.article_list = res;
+    
+  },data:()=> {
+    return {article_list:[]};
+  }
 }
 </script>
 
